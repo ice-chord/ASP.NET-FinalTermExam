ad103ee592f5fb66ac3acb198b74c091a9351538
ice_cord <1102137109@gm.kuas.edu.tw>
2016-06-23 16:14:04 +0800

    https://github.com/ice-chord/ASP.NET-FinalTermExam.get

---
+++ ASP.NET-FinalTermExam/Models/Code.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_FinalTermExam.Models
+{
+    public class Code
+    {
+        public string CodeId { get; set; }
+        public string CodeName { get; set; }
+    }
+}
